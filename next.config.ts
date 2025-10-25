c5efe04667221452c0803fe95077b4c44e312d3f
Mert Bayça <103841104+mertbayca@users.noreply.github.com>
2025-10-26 00:23:17 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,14 @@ import { withContentlayer } from "next-contentlayer2";
 const nextConfig: NextConfig = {
   experimental: {
     mdxRs: true,
+    serverComponentsExternalPackages: ["@prisma/client", "@prisma/adapter-neon", "@neondatabase/serverless"],
   },
-  turbopack: {},
+  webpack: (config) => {
+    if (Array.isArray(config.externals)) {
+      config.externals.push("@prisma/client", "@prisma/adapter-neon", "@neondatabase/serverless");
+    }
+    return config;
+  },
 };
 
 export default withContentlayer(nextConfig);
